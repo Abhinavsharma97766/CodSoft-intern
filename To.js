b0c00c90cde6bc4f5c416d07d6b3fb755663db7b
Abhinavsharma97766 <112763284+Abhinavsharma97766@users.noreply.github.com>
2023-10-15 14:24:24 +0530

    Add files via upload

---
+++ To.js
@@ -0,0 +1,7 @@
+// select everything
+// select the todo-form
+const todoForm = document.querySelector('.todo-form');
+// select the input box
+const todoInput = document.querySelector('.todo-input');
+// select the <ul> with class="todo-items"
+const todoItemsList = document.querySelector('.todo-items');
